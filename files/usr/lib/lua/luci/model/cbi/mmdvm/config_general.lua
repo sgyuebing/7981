62d6b6fcad9166a1486da0aef4a9630dd148b348
qzyuebing <54806519+qzyuebing@users.noreply.github.com>
2025-07-02 16:18:41 +0800

    Update config_general.lua

--- files/usr/lib/lua/luci/model/cbi/mmdvm/config_general.lua
+++ files/usr/lib/lua/luci/model/cbi/mmdvm/config_general.lua
@@ -1,64 +1,93 @@
-local n=require"luci.sys"
-local o=require"nixio.fs"
-local e=require"luci.jsonc"
-local a,t,e=...
-local i=require("luci.model.mmdvm")
-local s=require("luci.http")
-a=Map("mmdvm","",translate("Here you can configure the basic aspects of your device like its callsign or the operating modes."))
-a.on_after_commit=function(e)
-if e.changed then
-changes=e.uci:changes("mmdvm")
-if i.uci2ini(changes)then
-n.call("env -i /etc/init.d/mmdvmhost restart >/dev/null")
+-- Copyright 2019-2020 Michael BD7MQB <bd7mqb@qq.com>
+-- This is free software, licensed under the GNU GENERAL PUBLIC LICENSE, Version 2.0
+
+local sys   = require "luci.sys"
+local fs    = require "nixio.fs"
+local json = require "luci.jsonc"
+
+local m, s, o = ...
+
+local mmdvm = require("luci.model.mmdvm")
+local http  = require("luci.http")
+-- local conffile = uci:get("mmdvm", "mmdvmhost", "conf") or "/etc/MMDVM.ini"
+
+m = Map("mmdvm", "", translate("Here you can configure the basic aspects of your device like its callsign or the operating modes."))
+m.on_after_commit = function(self)
+	if self.changed then	-- changes ?
+        changes = self.uci:changes("mmdvm")
+        -- if MMDVM.ini changed?
+        if mmdvm.uci2ini(changes) then
+            sys.call("env -i /etc/init.d/mmdvmhost restart >/dev/null")
+        end
+	end
 end
-end
-end
-i.ini2uci(a.uci)
-t=a:section(NamedSection,"General","mmdvmhost",translate("General Settings"))
-t.anonymous=true
-t.addremove=false
-e=t:option(Value,"Callsign",translate("Callsign"))
-e.optional=false
-e=t:option(Value,"Id",translate("ID"),translate("Your DmrId or DmrId + <abbr title=\"ex. 460713301\">2 digitals</abbr>"))
-e.optional=true
-e.datatype="uinteger"
-e=t:option(ListValue,"Duplex",translate("Duplex/Simplex"))
-e:value("0",translate("Simplex"))
-e:value("1",translate("Duplex"))
-e=t:option(Value,"NetModeHang",translate("NetModeHang"))
-e.datatype="uinteger"
-e=t:option(Value,"RFModeHang",translate("RFModeHang"))
-e.datatype="uinteger"
-t=a:section(NamedSection,"Info","mmdvmhost",translate("Infomation Settings"),translate("Those infomation will show up at brandmeister.network if you enable DMR mode"))
-t.anonymous=true
-e=t:option(Value,"RXFrequency",translate("RXFrequency"),translate("Use the format <abbr title=\"the Unit is Hz\">434500000</abbr>, in Hz"))
-e.optional=true
-e.datatype="uinteger"
-e=t:option(Value,"TXFrequency",translate("TXFrequency"),translate("Use the same format as RXFrequency"))
-e.optional=true
-e.datatype="uinteger"
-e=t:option(Value,"Latitude",translate("Latitude"),translate("e.g. 22.10 N"))
-e=t:option(Value,"Longitude",translate("Longitude"),translate("e.g. 114.3 E"))
-e=t:option(Value,"Height",translate("Height"),translate("e.g. 110 Meters"))
-e.optional=true
-e.datatype="uinteger"
-e=t:option(Value,"Power",translate("TXPower"),translate("e.g. 1 Watt"))
-e.optional=true
-e.datatype="uinteger"
-e=t:option(Value,"Location",translate("Location"))
-e=t:option(Value,"Description",translate("Description"))
-e=t:option(Value,"URL",translate("URL"))
-t=a:section(NamedSection,"Modem","mmdvmhost",translate("Modem Settings"))
-t.anonymous=true
-e=t:option(ListValue,"Port",translate("Port"),translate("The port of Modem"))
-e:value("NullModem","NullModem")
-if o.access("/dev/ttyS0")then e:value("/dev/ttyS0")end
-if o.access("/dev/ttyUSB0")then e:value("/dev/ttyUSB0")end
-if o.access("/dev/ttyUSB1")then e:value("/dev/ttyUSB1")end
-if o.access("/dev/ttyUSB2")then e:value("/dev/ttyUSB2")end
-if o.access("/dev/ttyACM0")then e:value("/dev/ttyACM0")end
-if o.access("/dev/ttyAMA0")then e:value("/dev/ttyAMA0")end
-e=t:option(Value,"RXOffset",translate("RXOffset"))
-e=t:option(Value,"TXOffset",translate("TXOffset"))
-e=t:option(Value,"RSSIMappingFile",translate("RSSIMappingFile"))
-return a
+
+-- Initialize uci file using ini if needed, MUST called at the fist run.
+mmdvm.ini2uci(m.uci)
+
+--
+-- General Properties
+--
+s = m:section(NamedSection, "General", "mmdvmhost", translate("General Settings"))
+s.anonymous = true
+s.addremove = false
+
+o = s:option(Value, "Callsign", translate("Callsign"))
+o.optional    = false
+
+o = s:option(Value, "Id", translate("ID"), translate("Your DmrId or DmrId + <abbr title=\"ex. 460713301\">2 digitals</abbr>"))
+o.optional    = true
+o.datatype    = "uinteger"
+
+o = s:option(ListValue, "Duplex", translate("Duplex/Simplex"))
+o:value("0", translate("Simplex"))
+o:value("1", translate("Duplex"))
+
+o = s:option(Value, "NetModeHang", translate("NetModeHang"))
+o.datatype    = "uinteger"
+o = s:option(Value, "RFModeHang", translate("RFModeHang"))
+o.datatype    = "uinteger"
+
+--
+-- Info Properties
+--
+s = m:section(NamedSection, "Info", "mmdvmhost", translate("Infomation Settings"), translate("Those infomation will show up at brandmeister.network if you enable DMR mode"))
+s.anonymous   = true
+
+o = s:option(Value, "RXFrequency", translate("RXFrequency"), translate("Use the format <abbr title=\"the Unit is Hz\">434500000</abbr>, in Hz"))
+o.optional    = true
+o.datatype    = "uinteger"
+o = s:option(Value, "TXFrequency", translate("TXFrequency"), translate("Use the same format as RXFrequency"))
+o.optional    = true
+o.datatype    = "uinteger"
+o = s:option(Value, "Latitude", translate("Latitude"), translate("e.g. 22.10 N"))
+o = s:option(Value, "Longitude", translate("Longitude"), translate("e.g. 114.3 E"))
+o = s:option(Value, "Height", translate("Height"), translate("e.g. 110 Meters"))
+o.optional    = true
+o.datatype    = "uinteger"
+o = s:option(Value, "Power", translate("TXPower"), translate("e.g. 1 Watt"))
+o.optional    = true
+o.datatype    = "uinteger"
+o = s:option(Value, "Location", translate("Location"))
+o = s:option(Value, "Description", translate("Description"))
+o = s:option(Value, "URL", translate("URL"))
+
+--
+-- Modem Properties
+--
+s = m:section(NamedSection, "Modem", "mmdvmhost", translate("Modem Settings"))
+s.anonymous   = true
+o = s:option(ListValue, "Port", translate("Port"), translate("The port of Modem"))
+o:value("NullModem", "NullModem")
+if fs.access("/dev/ttyS0") then o:value("/dev/ttyS0") end
+if fs.access("/dev/ttyUSB0") then o:value("/dev/ttyUSB0") end
+if fs.access("/dev/ttyUSB1") then o:value("/dev/ttyUSB1") end
+if fs.access("/dev/ttyACM0") then o:value("/dev/ttyACM0") end
+if fs.access("/dev/ttyAMA0") then o:value("/dev/ttyAMA0") end
+
+o = s:option(Value, "RXOffset", translate("RXOffset"))
+o = s:option(Value, "TXOffset", translate("TXOffset"))
+
+o = s:option(Value, "RSSIMappingFile", translate("RSSIMappingFile"))
+
+return m
